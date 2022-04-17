2a6065739213e84140c710b9dedd9f42ebd99927
liyazhuo <yli@liyazhuo.com>
2022-04-17 15:12:41 +0400

    Added comments/explaination of model. Added plot file

---
+++ numerical_simulations/plot.py
@@ -0,0 +1,23 @@
+from math import log
+import matplotlib.pyplot as plt
+
+
+#xlist = [0.1, 0.07, 0.03, 0.01, 0.007, 0.003, 0.001, 0.0007, 0.0003, 0.0001]
+x_list = [0.1, 0.04, 0.02, 0.01, 0.004, 0.002, 0.001, 0.0004, 0.0002, 0.0001]
+#ylist = [1.9628720274749862, 0.8210484760414204, 0.7044713239319443, 1.00420970409681, 0.9943519712259054, 1.00042437011137, 1.0003313606236395, 1.0000661062695848, 1.0000098288220713, 1.0000034656271268]
+y_list = [1.9628720274749862, 0.49905207794877127, 0.8762215772023585, 1.00420970409681, 1.0041638199498022, 1.0012317378226068, 1.0003313606236395, 1.0000551300106866, 1.000013903900782, 1.0000034656271268]
+xnewlist = []
+ynewlist = []
+for i in x_list:
+    i = -log(i, 10)
+    xnewlist.append(i)
+#print (xnewlist)
+for i in y_list:
+    i = log(abs(i-1.0), 10)
+    ynewlist.append(i)
+#print (ynewlist)
+plt.plot(xnewlist, ynewlist, "ro")
+plt.xlabel("log of ratio")
+plt.ylabel("negative log of time steps ")
+plt.savefig("different_time_step.png", dpi = 300)
+plt.show()
--- numerical_simulations/spacecraft.py
+++ numerical_simulations/spacecraft.py
@@ -4,47 +4,42 @@ Created on Sat Mar 26 12:26:57 2022
 
 @author: desk_
 """
+
+from pickle import GLOBAL
 from vpython import *
 import pylab as plt
 
 #set up black hole
-M = 8.0*10**30
-G = 6.67*10**(-11)
-h = 1000000.0
-EventHorizon = sphere(pos=vector(0,0,0), radius = 11000.0, color=color.black)
-DangerZone = sphere(pos=vector(0,0,0), radius = 311000.0, color=color.red, opacity = 0.5)
+M = 8.0*10**30 #Mass of the black hole
+G = 6.67*10**(-11) #gravitational constant
+h = 1000000.0 #initial distance from black hole
+EventHorizon = sphere(pos=vector(0,0,0), radius = 11000.0, color=color.black) #Position and size of the black hole
+DangerZone = sphere(pos=vector(0,0,0), radius = 311000.0, color=color.red, opacity = 0.5) #Spaceship will be torn apart within this zone
 
 # Set up spaceship
 
-theta = 90*pi/180.0
-v0 = 16000000.0 
-#v0 = sqrt(G*M/h)
+theta = 90*pi/180.0 #inital angle
+#v0 = 16000000.0 #Inital velocity of spaceship
+v0 = sqrt(G*M/h) #Inital velocity of spaceship needed for a perfect circular orbit around the black hole 
 ship = sphere(pos = vector(h,0,0), vel=vector(v0*cos(theta),v0*sin(theta),0), radius = 20000.0, color=color.yellow)
 
 #print (v0)
 
-#set up graph
-#myplot = graph(xtitle = "Time (s)", ytitle = "Height (m)", fast = False)
-#line = gcurve(color = color.red )
-
 # Move spaceship via loop
-t = 0.0
-dt = 0.0001
-t_full = 2*pi*h/v0
-
-#print(mag(ship.vel))
-#print(mag(ship.pos))
+t = 0.0 
+dt = 0.04 #time step
+t_full = 2*pi*h/v0 #running time for orbit of a full circle
 
 
-while t < 0.1:
+while t < t_full:
     rate(100)
     t = t + dt
-    a = -G*M/(mag(ship.pos)**2)*norm(ship.pos)
+    a = -G*M/(mag(ship.pos)**2)*norm(ship.pos) #acceleration of the ship due to gravity
     ship.vel = ship.vel + a*dt
     ship.pos = ship.pos + ship.vel*dt 
 
 
-ratio=(0.5*mag(ship.vel)**2 + G*M/mag(ship.pos))/( 0.5*v0**2 + G*M/h)
+ratio=(0.5*mag(ship.vel)**2 + G*M/mag(ship.pos))/( 0.5*v0**2 + G*M/h) #Ratio of final energy and initial energy of spaceship, if simulation is perfect, this should be 1.
 
 #print(mag(ship.vel))
 #print(mag(ship.pos))
